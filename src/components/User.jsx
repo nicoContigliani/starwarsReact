b6c6a663f5bc67657158df25b322a6018a792a24
nico <nico.contigliani@gmail.com>
2021-03-28 20:06:15 -0300

    mapeo chau ternario

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from "axios";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    NavLink
-} from "react-router-dom";
+// import {
+//     BrowserRouter as Router,
+//     Switch,
+//     Route,
+//     Link,
+//     NavLink
+// } from "react-router-dom";
 
 
 
@@ -32,6 +32,10 @@ const User = () => {
 
     useEffect(() => {
         getDatos()
+        if (per.length === 0 || per ==="") {
+            // alert("per cero")
+            setPercero(true)
+        }
     }, [])
 
 
@@ -41,33 +45,28 @@ const User = () => {
         const req = await axios.get(`${url}${id}`);
         const reqs = req.data;
         setPer(reqs);
+        
 
         // console.log("no hay id");
 
+    }
+    const getSecondsDate = async()=>{
 
-
-
-        setTimeout(async () => {
-            if (per.length === 0) {
-                // alert("per cero")
-                setPercero(true)
-            } else {
-
-
+           
+    
                 const f = await per.films.map((item) => (
                     `${item}`
                 ))
-
+    
                 const fi = await (f.join('  -  '));
                 setFilms(fi)
-
+    
                 const s = await per.species.map((item) => (
                     `${item}`
                 ))
                 const Si = await (s.join('  -  '));
                 setSpecies(Si)
-
-
+    
                 const n = await per.starships.map((item) => (
                     `${item}`
                 ))
@@ -78,24 +77,24 @@ const User = () => {
                 ))
                 const Vi = await (v.join('  -  '));
                 setVehicles(Vi)
-            }
+            
+
+    }    
+   
 
 
 
 
 
 
-        }, 2000);
-
-        console.log(per);
-    }
+        // console.log(per);
+    
 
     return (
         <div className="container">
 
 
             {
-                perCero == true ?
 
 
                     <div key={id}>
@@ -110,20 +109,18 @@ const User = () => {
                             <div className="container">
 
 
-                       Cumpleaños:{per.birth_year}<br /><hr/>
-                       Creado:{per.created}<br /><hr/>
-                       Editado:{per.edited}<br /><hr/>
-                       Color de ojos: {per.eye_color}<br /><hr/>
-                       Color de pelo: {per.hair_color}<br/><hr/>
-                       Color de piel: {per.skin_color} <br/><hr/>
-                       Genero: {per.gender} <br /><hr/>
-
-                         {per.starships.length === 0 ? "" :  `Nave Estelar: ${per.starships} `}<hr/> 
-                         {per.vehicles.length === 0 ? "" : `Vehiculo: ${per.vehicles} `} <hr/>
-                         {per.films.length === 0 ? "" :  `Peliculas: ${per.films} `} <hr/>
-                         {per.films.species === 0 ||per.films.species === ""  ? "" :  `Especie: ${per.species} `} 
-
-
+                         Cumpleaños:{per.birth_year}<br /><hr/>
+                         Creado:{per.created}<br /><hr/>
+                         Editado:{per.edited}<br /><hr/>
+                         Color de ojos: {per.eye_color}<br /><hr/>
+                         Color de pelo: {per.hair_color}<br/><hr/>
+                         Color de piel: {per.skin_color} <br/><hr/>
+                         Genero: {per.gender} <br /><hr/>
+                      
+                         Peliculas:<br />{per.films} <br /> <hr/>
+                         Nave Estelar:<br /> {per.starships} <br /> <hr/>
+                         Vehiculo:<br /> {per.vehicles} <br/><hr/>
+                          Especie:<br /> {per.species}     <br /> <hr/>
 
 
 
@@ -139,36 +136,6 @@ const User = () => {
 
 
 
-                    :
-
-
-                    <div key={id}>
-
-
-
-                        <div className="starwars">                     
-
-                            <div className="container">
-                                Nombre:<br />
-                                {per.name}<br />
-                                 Cumpleaños:<br /> {per.birth_year}<br />
-                                 Creado:<br /> {per.created}<br />
-                                 Editado:<br /> {per.edited}<br />
-                                 Color de ojos:<br /> {per.eye_color}<br />
-                                 Color de pelo:<br /> {per.hair_color}
-                                 Color de piel:<br /> {per.skin_color}
-                                <br />
-                                 Peliculas:<br />{per.films} <br />
-                                 Nave Estelar:<br /> {per.starships} <br />
-                                 Vehiculo:<br /> {per.vehicles}
-                                 Especie:<br /> {per.species}     <br />
-                                 Genero: {per.gender} <br />
-
-                            </div>
-                        </div>
-
-
-                    </div>
             }
         </div>
     )
