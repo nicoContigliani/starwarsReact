5524695915b823d795992cdb76fd8fc74c8dad75
nico <nico.contigliani@gmail.com>
2021-03-28 23:13:09 -0300

    contacto

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -3,7 +3,25 @@ import React from 'react'
 const Contacto = () => {
     return (
         <div>
-            con
+            <br />
+            <div className="container">
+                <h1>Contacto </h1>
+                <div className="caja">
+                    <br/><hr/>
+                    
+                <h2>Teléfono</h2>
+                 <h3>2612444106</h3>
+                 <br/><hr/>
+
+                 <h2>Linkedin</h2>
+                 <h3>www.linkedin.com/in/nicolas-contigliani</h3>
+                 <br/><hr/>
+
+                </div>
+
+
+
+            </div>
         </div>
     )
 }
